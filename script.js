59592b7d41626ccd12cc084a003b78f42202b5aa
PVS2008 <144795083+PVS2008@users.noreply.github.com>
2024-08-09 18:47:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Exemplo de mensagem ao enviar formulário
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+});
